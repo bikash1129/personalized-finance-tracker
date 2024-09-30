6d44bf93c81298d2d5e8b7f9b30db516ab43c86d
bikash1129 <santrabikash29@gmail.com>
2024-09-30 15:23:16 +0530

    hello12345

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,9 @@
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import { Dashboard } from "./pages/dashboard";
-// import { Auth } from "./pages/auth";
-// import { FinancialRecordsProvider } from "./contexts/financial-record-context";
-// import { SignedIn, UserButton } from "@clerk/clerk-react";
-// // import { dark } from "@clerk/themes";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="app-container">
-//         <div className="navbar">
-//           <Link to="/"> Dashboard</Link>
-//           <SignedIn>
-//             <UserButton />
-//           </SignedIn>
-//         </div>
-//         <Routes>
-//         <Route path="/" element={<Auth />} />
-//           <Route
-//             path="/dashboard"
-//             element={
-//               <FinancialRecordsProvider>
-//                 <Dashboard />
-//               </FinancialRecordsProvider>
-//             }
-
-//           />
-          
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
 import { FinancialRecordsProvider } from "./contexts/financial-record-context";
-import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 // import { dark } from "@clerk/themes";
 
 function App() {
@@ -50,8 +11,8 @@ function App() {
     <Router>
       <div className="app-container">
         <div className="navbar">
+          <Link to="/auth"> Dashboard</Link>
           <SignedIn>
-            <Link to="/dashboard">Dashboard</Link>
             <UserButton />
           </SignedIn>
         </div>
@@ -59,23 +20,12 @@ function App() {
           <Route
             path="/"
             element={
-              <SignedOut>
-                <Auth />
-              </SignedOut>
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
             }
           />
-          <Route
-            path="/dashboard"
-            element={
-              <SignedIn>
-                <FinancialRecordsProvider>
-                  <Dashboard />
-                </FinancialRecordsProvider>
-              </SignedIn>
-            }
-          />
-          {/* Redirect unauthenticated users to the Auth page */}
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
     </Router>
