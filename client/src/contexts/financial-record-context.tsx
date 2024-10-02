395637b1c6ef52ddacf09789f1ab63af73d7910c
bikash1129 <santrabikash29@gmail.com>
2024-10-02 23:38:09 +0530

    push

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -33,7 +33,7 @@ export const FinancialRecordsProvider = ({
   const fetchRecords = async () => {
     if (!user) return;
     const response = await fetch(
-      `https://personalized-finance-tracker.onrender.com/financial-records/getAllByUserID/${user.id}`
+      `https://personalized-finance-tracker-1.onrender.com/financial-records/getAllByUserID/${user.id}`
     );
 
     if (response.ok) {
@@ -48,7 +48,7 @@ export const FinancialRecordsProvider = ({
   }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
-    const response = await fetch("http://localhost:3001/financial-records", {
+    const response = await fetch("https://personalized-finance-tracker-1.onrender.com/financial-records", {
       method: "POST",
       body: JSON.stringify(record),
       headers: {
@@ -66,7 +66,7 @@ export const FinancialRecordsProvider = ({
 
   const updateRecord = async (id: string, newRecord: FinancialRecord) => {
     const response = await fetch(
-      `http://localhost:3001/financial-records/${id}`,
+      `https://personalized-finance-tracker-1.onrender.com/financial-records/${id}`,
       {
         method: "PUT",
         body: JSON.stringify(newRecord),
@@ -94,7 +94,7 @@ export const FinancialRecordsProvider = ({
 
   const deleteRecord = async (id: string) => {
     const response = await fetch(
-      `http://localhost:3001/financial-records/${id}`,
+      `https://personalized-finance-tracker-1.onrender.com/financial-records/${id}`,
       {
         method: "DELETE",
       }
